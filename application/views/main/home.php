8c223768e6ae8e767b88837827fc24c96fff369f
Lokesh <lokesh.jain@gympik.com>
2015-12-05 17:54:48 +0530

    whole code of website

---
+++ application/views/main/home.php
@@ -0,0 +1,62 @@
+<section class="slider" id="home">
+	<div class="container-fluid">
+		<div class="row">
+	
+		    <div id="carouselHacked" class="carousel slide carousel-fade" data-ride="carousel">
+				<div class="header-backup"></div>
+		        <!-- Wrapper for slides -->
+		        <div class="carousel-inner" role="listbox">
+		            <div class="item active">
+		            	<img src="<?php echo base_url();?>img/slide-one.jpg" alt="">
+		                <div class="carousel-caption">
+	               			<h1>Tax</h1>
+	               			<p>good business consulting service</p>
+	               			<button>learn more</button>
+		                </div>
+		            </div>
+		            <div class="item">
+		            	<img src="<?php echo base_url();?>img/slide-two.jpg" alt="">
+		                <div class="carousel-caption">
+	               			<h1>Financial</h1>
+	               			<p>good business consulting service</p>
+	               			<button>learn more</button>
+		                </div>
+		            </div>
+		            <div class="item">
+		            	<img src="<?php echo base_url();?>img/slide-three.jpg" alt="">
+		                <div class="carousel-caption">
+	               			<h1>Consulting</h1>
+	               			<p>good business consulting service</p>
+	               			<button>learn more</button>
+		                </div>
+		            </div>
+		            <div class="item">
+		            	<img src="<?php echo base_url();?>img/slide-four.jpg" alt="">
+		                <div class="carousel-caption">
+	               			<h1>Money</h1>
+	               			<p>good business consulting service</p>
+	               			<button>learn more</button>
+		                </div>
+		            </div>
+		        </div>
+	
+		        <!-- Controls -->
+		        <a class="left carousel-control" href="#carouselHacked" role="button" data-slide="prev">
+		            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		            <span class="sr-only">Previous</span>
+		        </a>
+		        <a class="right carousel-control" href="#carouselHacked" role="button" data-slide="next">
+		            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		            <span class="sr-only">Next</span>
+		        </a>
+		    </div>
+	
+		</div>
+	</div>
+</section><!-- end of slider section -->
+<script src="js/jquery-2.1.1.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="<?php echo base_url(); ?>js/gmaps.js"></script>
+	<script src="<?php echo base_url(); ?>js/smoothscroll.js"></script>
+	<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/custom.js"></script>
